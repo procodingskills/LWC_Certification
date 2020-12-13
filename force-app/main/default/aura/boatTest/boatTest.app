7cb5be0a5c2be86563764e9a8b6a101aea561239
LakshmiDeepak <i.lakshmideepak@gmail.com>
2020-12-13 15:29:32 +0530

    Lightning Web Component Super Badge

---
+++ force-app/main/default/aura/boatTest/boatTest.app
@@ -0,0 +1,3 @@
+<aura:application extends="force:slds">
+    <c:boatSearchForm/>
+</aura:application>
--- force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -1,13 +1,3 @@
-/**
- * @description       : 
- * @author            : i.lakshmideepak@gmail.com
- * @group             : 
- * @last modified on  : 12-11-2020
- * @last modified by  : i.lakshmideepak@gmail.com
- * Modifications Log 
- * Ver   Date         Author                      Modification
- * 1.0   12-11-2020   i.lakshmideepak@gmail.com   Initial Version
-**/
 import { LightningElement, wire, track } from "lwc";
 import getBoatTypes from "@salesforce/apex/BoatDataService.getBoatTypes";
 export default class BoatSearchForm extends LightningElement {
